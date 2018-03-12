707efac102dfa9adfbce8048503ccfa1932d8216
Ed Baker <edwbaker@gmail.com>
2018-03-12 14:38:08 +0000

    Dev update.

--- oscillo/oscillo.php
+++ oscillo/oscillo.php
@@ -29,6 +29,22 @@ function oscillo_init() {
   return ($init);
 }
 
+function oscillo_prepare() {
+  core_log("info", "oscillo", "Attempting to list oscillogram image files on analysis server.");
+  exec("s3cmd ls s3://bioacoustica-analysis/oscillo/".$GLOBALS["modules"]["oscillo"]["git_hash"]."/", $output, $return_value);
+  if ($return_value == 0) {
+    if (count($output) == 0) {
+      $GLOBALS["oscillo"]["oscillograms"] = array();
+    } else {
+      foreach ($output as $line) {
+        $start = strrpos($line, "/");
+        $GLOBALS["oscillo"]["oscillograms"][] = substr($line, $start + 1);
+      }
+    }
+  core_log("info", "bioacoustica", count($GLOBALS["oscillo"]["oscillograms"])." oscillogram image files found.");
+  }
+}
+
 function oscillo_analyse($recording) {
   $return = array();
   $file = core_download("wav/".$recording["id"].".wav");
@@ -37,5 +53,18 @@ function oscillo_analyse($recording) {
     "local path" => "scratch/wav/",
     "save path" => NULL
   );
+  
+  //BELOW COPIED FROM OLD ANALYSIS
+  
+  if (file_exists("./oscillo/$recording_id".".png")) {
+    print "Skipping file already plotted.".PHP_EOL;
+  } else {
+    $url = "https://ams3.digitaloceanspaces.com/bioacoustica-data-backup/recordings/$filename";
+    system ("wget -O $filename $url");
+    $format = strrchr(".", $filename);
+    system("Rscript oscillo.R $recording_id '$species' $filename");
+    system("rm $filename");
+  }
+  
   return($return);
 }
